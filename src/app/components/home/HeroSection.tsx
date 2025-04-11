b6683a1913164a12efe2bbc049b66cfb0b2cd797
liljdot <jehuoki@gmail.com>
2025-04-11 14:00:46 +0100

    Update HeroSection styles for improved responsiveness and spacing for tablet

--- src/app/components/home/HeroSection.tsx
+++ src/app/components/home/HeroSection.tsx
@@ -6,10 +6,10 @@ const HeroSection: React.FC = () => {
 
     return (
         <>
-            <section style={{ backgroundImage: `linear-gradient(to bottom, #000000EE, #00000000), url(${heroBG.src})` }} className="bg-cover bg-center pt-17 xl:pt-55 pb-12.5 xl:pb-37">
-                <div className="flex flex-col gap-4 xl:gap-10">
+            <section style={{ backgroundImage: `linear-gradient(to bottom, #000000EE, #00000000), url(${heroBG.src})` }} className="bg-cover bg-center pt-17 md:pt-24 lg:pt-40 xl:pt-55 pb-12.5 md:pb-16 lg:pb-24 xl:pb-37">
+                <div className="flex flex-col gap-4 md:gap-6 lg:gap-8 xl:gap-10">
                     <div className="flex flex-col gap-5 xl:gap-15 text-neutral text-center">
-                        <h1 className="text-3xl xl:text-[105px] xl:font-medium px-5 xl:px-18">Building Dreams, One Brick at a Time.</h1>
+                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-[105px] xl:font-medium px-5 xl:px-18">Building Dreams, One Brick at a Time.</h1>
                         <p className={"text-sm xl-text-base px-16 xl:px-31 " + openSans.className}>
                             Supplying quality cement and building materials to turn visions into reality.
                             <br />
