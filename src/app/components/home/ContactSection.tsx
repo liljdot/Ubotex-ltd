2849e13753e066784a39beb13b9bc69ee60b65f7
liljdot <jehuoki@gmail.com>
2025-04-14 20:32:15 +0100

    Refactor ContactSection layout for improved responsiveness and styling

--- src/app/components/home/ContactSection.tsx
+++ src/app/components/home/ContactSection.tsx
@@ -7,11 +7,13 @@ const ContactSection: React.FC = () => {
 
     return (
         <>
-            <section>
-                <div className="size-full">
+            <section className="flex flex-col gap-16 xl:gap-50">
+            <h1 className="text-primary text-xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-semibold opacity-80 text-center">CONTACT US</h1>
+
+                <div className="h-50">
                     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
                         <Map
-                            style={{ width: '100vw', height: '100vh' }}
+                            style={{ width: '100%', height: '100%' }}
                             defaultCenter={position}
                             center={position}
                             defaultZoom={18}
