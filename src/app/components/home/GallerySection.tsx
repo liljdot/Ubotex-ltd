89525b65ca99875fda914d17c5e7e03957764a14
liljdot <jehuoki@gmail.com>
2025-04-15 19:10:42 +0100

    Update GallerySection layout to improve image grid responsiveness and visibility

--- src/app/components/home/GallerySection.tsx
+++ src/app/components/home/GallerySection.tsx
@@ -24,7 +24,7 @@ const GallerySection: React.FC = () => {
             <section className="flex flex-col gap-4 md:gap-8 lg:gap-12 xl:gap-15 py-15 md:py-20 lg:py-30 xl:py-40">
                 <h1 className="text-primary text-xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-semibold opacity-80 text-center">OUR GALLERY</h1>
 
-                <div className="grid grid-flow-col grid-rows-2 auto-cols-[136px] xl:auto-cols-[360px] auto-rows-[136px] xl:auto-rows-[360px] gap-0 overflow-scroll">
+                <div className="grid grid-flow-col grid-rows-1 lg:grid-rows-2 auto-cols-[264px] lg:auto-cols-[360px] auto-rows-[264px] lg:auto-rows-[360px] gap-0 overflow-scroll">
                     {images.map((image: any, index: number) => <GalleryImage key={`gallery-image-${index}`} image={image} />)}
                 </div>
             </section>
